21e74197d51a7a33ac95286cd6522bbd0a26b8e5
meteorlxy <meteor.lxy@foxmail.com>
2021-10-20 18:58:20 +0800

    feat(eslint-config): enable sort-imports rule

--- packages/eslint-config/lib/index.js
+++ packages/eslint-config/lib/index.js
@@ -54,6 +54,22 @@ module.exports = {
     /**
      * Override airbnb-base rules to ensure import order
      *
+     * @see https://eslint.org/docs/rules/sort-imports
+     * @see https://github.com/airbnb/javascript/blob/4ee732639396362c6981b1aa4730934c3baa7919/packages/eslint-config-airbnb-base/rules/es6.js#L165-L172
+     */
+    'sort-imports': [
+      'error',
+      {
+        ignoreCase: true,
+        ignoreDeclarationSort: true,
+        ignoreMemberSort: false,
+        allowSeparatedGroups: false,
+      },
+    ],
+
+    /**
+     * Override airbnb-base rules to ensure import order
+     *
      * @see https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
      * @see https://github.com/airbnb/javascript/blob/1ca21aba799699ba556bed058e3900514a9fbee3/packages/eslint-config-airbnb-base/rules/imports.js#L144-L147
      */
