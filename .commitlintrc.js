47fc0d09e9af0eb66d8217289cd900d9f512e39f
meteorlxy <meteor.lxy@foxmail.com>
2022-05-28 14:30:41 +0800

    build: update lint-staged config

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -1,16 +1,12 @@
-const fs = require('fs')
-const path = require('path')
+const fs = require('fs');
+const path = require('path');
 
-const packages = fs.readdirSync(path.resolve(__dirname, 'packages'))
+const packages = fs.readdirSync(path.resolve(__dirname, 'packages'));
 
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'scope-enum': [
-      2,
-      'always',
-      packages,
-    ],
+    'scope-enum': [2, 'always', packages],
     'footer-max-line-length': [0],
   },
-}
+};
