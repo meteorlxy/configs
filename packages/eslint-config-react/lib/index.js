c6ad7adb01a8f394506af4961b5117458aa7fbb9
meteorlxy <meteor.lxy@foxmail.com>
2022-06-02 17:01:42 +0800

    feat(eslint-config-react): enforce arrow-function for function components

--- packages/eslint-config-react/lib/index.js
+++ packages/eslint-config-react/lib/index.js
@@ -15,5 +15,19 @@ module.exports = {
   // Rules overrides
   rules: {
     ...baseConfig.rules,
+
+    /**
+     * Enforce arrow function for function components
+     *
+     * @see https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/function-component-definition.md
+     * @see https://github.com/airbnb/javascript/blob/cbf9ade10a2f6f06c9da6dbfa25b344bee4bbef6/packages/eslint-config-airbnb/rules/react.js#L525-L530
+     */
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
   },
 };
