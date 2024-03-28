2d804e026a335b4cce5d19e4c7294f538be0c56f
meteorlxy <meteor.lxy@foxmail.com>
2024-03-28 12:08:09 +0800

    chore: tweak code ordering

--- packages/eslint-config/src/rules/typescript-shim.ts
+++ packages/eslint-config/src/rules/typescript-shim.ts
@@ -2,6 +2,44 @@ import type { FlatConfig } from '@typescript-eslint/utils/ts-eslint';
 
 export const typescriptShimRules = {
   /**
+   * Rules that are checked by typescript or recommended to disable in typescript.
+   */
+  'constructor-super': 'off',
+  'getter-return': 'off',
+  'no-const-assign': 'off',
+  'no-dupe-args': 'off',
+  'no-dupe-class-members': 'off',
+  'no-dupe-keys': 'off',
+  'no-func-assign': 'off',
+  'no-invalid-this': 'off',
+  'no-import-assign': 'off',
+  'no-new-symbol': 'off',
+  'no-obj-calls': 'off',
+  'no-redeclare': 'off',
+  'no-setter-return': 'off',
+  'no-this-before-super': 'off',
+  'no-undef': 'off',
+  'no-unreachable': 'off',
+  'no-unsafe-negation': 'off',
+  'valid-typeof': 'off',
+
+  /**
+   * Rules from eslint-plugin-import that should be disabled
+   *
+   * @see https://typescript-eslint.io/troubleshooting/performance-troubleshooting#eslint-plugin-import
+   */
+  'import/default': 'off',
+  'import/named': 'off',
+  'import/namespace': 'off',
+  'import/no-named-as-default-member': 'off',
+  'import/no-unresolved': 'off',
+  // might run them in CI
+  'import/no-cycle': 'off',
+  'import/no-deprecated': 'off',
+  'import/no-named-as-default': 'off',
+  'import/no-unused-modules': 'off',
+
+  /**
    * Rules that have equivalents in typescript-eslint plugin.
    */
   'brace-style': 'off',
@@ -48,43 +86,4 @@ export const typescriptShimRules = {
   'space-before-blocks': 'off',
   'space-before-function-paren': 'off',
   'space-infix-ops': 'off',
-
-  /**
-   * Rules that are checked by typescript or recommended to disable in typescript.
-   */
-  // builtin rules
-  'constructor-super': 'off',
-  'getter-return': 'off',
-  'no-const-assign': 'off',
-  'no-dupe-args': 'off',
-  'no-dupe-class-members': 'off',
-  'no-dupe-keys': 'off',
-  'no-func-assign': 'off',
-  'no-invalid-this': 'off',
-  'no-import-assign': 'off',
-  'no-new-symbol': 'off',
-  'no-obj-calls': 'off',
-  'no-redeclare': 'off',
-  'no-setter-return': 'off',
-  'no-this-before-super': 'off',
-  'no-undef': 'off',
-  'no-unreachable': 'off',
-  'no-unsafe-negation': 'off',
-  'valid-typeof': 'off',
-
-  /**
-   * Rules from eslint-plugin-import that should be disabled
-   *
-   * @see https://typescript-eslint.io/troubleshooting/performance-troubleshooting#eslint-plugin-import
-   */
-  'import/default': 'off',
-  'import/named': 'off',
-  'import/namespace': 'off',
-  'import/no-named-as-default-member': 'off',
-  'import/no-unresolved': 'off',
-  // might run them in CI
-  'import/no-cycle': 'off',
-  'import/no-deprecated': 'off',
-  'import/no-named-as-default': 'off',
-  'import/no-unused-modules': 'off',
 } satisfies FlatConfig.Rules;
