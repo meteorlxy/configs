62eb6606c662c528b8556f6f6e788bbf48b60b82
meteorlxy <meteor.lxy@foxmail.com>
2024-04-01 00:17:44 +0800

    feat(eslint-config): add some vue rules

--- packages/eslint-config/src/rules/vue.ts
+++ packages/eslint-config/src/rules/vue.ts
@@ -7,6 +7,8 @@ export const vueRules = {
       order: ['script', 'template', 'style'],
     },
   ],
+  'vue/camelcase': 'off', // typescript
+  'vue/component-api-style': ['error', ['script-setup', 'composition']],
   'vue/component-name-in-template-casing': [
     'error',
     'PascalCase',
@@ -17,10 +19,27 @@ export const vueRules = {
   ],
   'vue/component-options-name-casing': ['error', 'PascalCase'],
   'vue/component-tags-order': 'off', // deprecated
+  'vue/custom-event-name-casing': [
+    'error',
+    'camelCase',
+    {
+      ignores: [],
+    },
+  ],
+  'vue/define-emits-declaration': ['error', 'type-literal'],
   'vue/define-macros-order': [
     'error',
     {
       order: ['defineOptions', 'defineProps', 'defineEmits', 'defineSlots'],
+    },
+  ],
+  'vue/define-props-declaration': ['error', 'type-based'],
+  'vue/dot-notation': 'off', // typescript
+  'vue/eqeqeq': [
+    'error',
+    'always',
+    {
+      null: 'ignore',
     },
   ],
   'vue/match-component-file-name': [
@@ -30,13 +49,19 @@ export const vueRules = {
       shouldMatchCase: false,
     },
   ],
+  'vue/match-component-import-name': 'error',
+  'vue/no-invalid-model-keys': 'off', // deprecated
+  'vue/no-ref-object-destructure': 'off', // deprecated
   'vue/no-restricted-syntax': [
     'error',
     'DebuggerStatement',
     'LabeledStatement',
     'WithStatement',
   ],
+  'vue/no-setup-props-destructure': 'off', // deprecated
   'vue/no-sparse-arrays': 'error',
   'vue/no-unused-refs': 'error',
   'vue/no-useless-v-bind': 'error',
+  'vue/script-setup-uses-vars': 'off', // deprecated
+  'vue/v-on-function-call': 'off', // deprecated
 } satisfies FlatConfig.Rules;
