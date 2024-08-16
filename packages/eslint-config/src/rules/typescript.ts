a29771787139b208a5a13153ee7dae874b15fe24
meteorlxy <meteor.lxy@foxmail.com>
2024-08-16 10:29:48 +0800

    fix(eslint-config): turn off no-invalid-void-type

--- packages/eslint-config/src/rules/typescript.ts
+++ packages/eslint-config/src/rules/typescript.ts
@@ -102,6 +102,7 @@ export const typescriptRules = {
   '@typescript-eslint/no-empty-interface': 'off', // deprecated
   '@typescript-eslint/no-import-type-side-effects': 'error',
   '@typescript-eslint/no-invalid-this': 'off', // checked by typescript
+  '@typescript-eslint/no-invalid-void-type': 'off', // @see https://github.com/typescript-eslint/typescript-eslint/issues/8113
   '@typescript-eslint/no-redeclare': 'off', // checked by typescript
   '@typescript-eslint/no-require-imports': ['error', { allow: [] }],
   '@typescript-eslint/no-restricted-imports': [
