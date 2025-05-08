b94b14116647781b903ed6027c4b13096187cb5d
meteorlxy <meteor.lxy@foxmail.com>
2025-05-09 00:21:27 +0800

    feat(eslint-config): enable no-unnecessary-type-conversion rule

--- packages/eslint-config/src/rules/typescript.ts
+++ packages/eslint-config/src/rules/typescript.ts
@@ -138,6 +138,7 @@ export const typescriptRules = {
   '@typescript-eslint/no-unnecessary-parameter-property-assignment': 'error',
   '@typescript-eslint/no-unnecessary-qualifier': 'off',
   '@typescript-eslint/no-unnecessary-type-assertion': 'warn',
+  '@typescript-eslint/no-unnecessary-type-conversion': 'warn',
   '@typescript-eslint/no-unsafe-argument': 'warn',
   '@typescript-eslint/no-unsafe-assignment': 'warn',
   '@typescript-eslint/no-unsafe-member-access': 'warn',
